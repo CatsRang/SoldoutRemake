03eaf01dfc73b7545425e57b02bd0dc75c215512
HyunWoo Lee <bless2k@gmail.com>
2022-06-24 23:53:08 +0900

    initial version

---
+++ bin/so_remake.dart
@@ -0,0 +1,5 @@
+import 'package:so_remake/so_remake.dart' as so_remake;
+
+void main(List<String> arguments) {
+  print('Hello world: ${so_remake.calculate()}!');
+}
---
+++ test/so_remake_test.dart
@@ -0,0 +1,8 @@
+import 'package:so_remake/so_remake.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
